51332f4a8a037d98e1c4fbe4ba948aa4e4c95655
Jack Belmont <jackbelmont8@gmail.com>
2017-12-04 16:42:45 -0500

    Added Floating Action Button

--- app/src/main/java/com/example/jackbelmont/travelmemories/MainActivity.java
+++ app/src/main/java/com/example/jackbelmont/travelmemories/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jackbelmont.travelmemories;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button addLocButton = (Button) findViewById(R.id.addLocButton);
+        addLocButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
