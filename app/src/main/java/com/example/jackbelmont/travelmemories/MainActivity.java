5e3b1160d0f3738d6847bcdcf9568e4a830f90e9
Thuc Nguyen <thucnguyen61098@gmail.com>
2017-12-04 17:00:12 -0500

    changed button to a regular button and added a textView

--- app/src/main/java/com/example/jackbelmont/travelmemories/MainActivity.java
+++ app/src/main/java/com/example/jackbelmont/travelmemories/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button addLocButton = (Button) findViewById(R.id.addLocButton);
-        addLocButton.setOnClickListener(new View.OnClickListener() {
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText("You have pressed the button!");
             }
         });
     }
